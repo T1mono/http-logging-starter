004d99bdf6e1faf2ac6eb55c0a9b99ab54bd2491
T1mono <167001770+T1mono@users.noreply.github.com>
2024-05-30 17:54:35 +0500

    git init

---
+++ src/main/java/ru/t1/opencschool/httploggingstarter/HttpLoggingStarterApplication.java
@@ -0,0 +1,12 @@
+package ru.t1.opencschool.httploggingstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpLoggingStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpLoggingStarterApplication.class, args);
+    }
+}
