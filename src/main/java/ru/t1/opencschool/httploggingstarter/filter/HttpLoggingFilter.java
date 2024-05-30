83b29e70093c0183f93a02eef443b9c3d9504132
T1mono <167001770+T1mono@users.noreply.github.com>
2024-05-30 18:14:07 +0500

    Added JavaDoc

--- src/main/java/ru/t1/opencschool/httploggingstarter/filter/HttpLoggingFilter.java
+++ src/main/java/ru/t1/opencschool/httploggingstarter/filter/HttpLoggingFilter.java
@@ -9,17 +9,30 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+/**
+ * Фильтр, который будет логировать HTTP запросы и ответы.
+ */
 @Slf4j
 public class HttpLoggingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         long startTime = System.currentTimeMillis();
-        String requestId = request.getHeader("X-Request-ID"); // You can use any unique identifier for each request
+        String requestId = request.getHeader("X-Request-ID"); // Можно использовать любой уникальный идентификатор для каждого запроса
 
-        log.info("Request ID: {}, Method: {}, URL: {}, Headers: {}", requestId, request.getMethod(), request.getRequestURI(), request.getHeaderNames());
+        log.info("Request ID: {}, Method: {}, URL: {}, Headers: {}",
+                requestId,
+                request.getMethod(),
+                request.getRequestURI(),
+                request.getHeaderNames()
+        );
 
         filterChain.doFilter(request, response);
 
-        log.info("Request ID: {}, Response Code: {}, Headers: {}, Time Taken: {}ms", requestId, response.getStatus(), response.getHeaderNames(), System.currentTimeMillis() - startTime);
+        log.info("Request ID: {}, Response Code: {}, Headers: {}, Time Taken: {}ms",
+                requestId,
+                response.getStatus(),
+                response.getHeaderNames(),
+                System.currentTimeMillis() - startTime
+        );
     }
 }
